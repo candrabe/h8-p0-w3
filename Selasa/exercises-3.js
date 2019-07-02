3ea9e70b88605179a9ebc168cbfd2da34b67484f
candra saputra <candrasaputra@live.com>
2019-07-02 13:10:34 +0700

    Melakukan Looping Data Array

---
+++ Selasa/exercises-3.js
@@ -0,0 +1,26 @@
+/*
+* Melakukan Looping Data Array
+* Sering kali data yang diterima dari database adalah array yang multidimensi (array di dalam array).
+* Sebagai developer, tugas kita adalah mengolah data tersebut agar dapat menampilkan informasi yang diinginkan.
+*/
+
+function dataHandling(arr){
+    for (let i = 0; i < arr.length; i++) {
+        console.log('No ID: ', arr[i][0]);
+        console.log('Nama Lengkap: ', arr[i][1]);
+        console.log('TTL: ', arr[i][2], arr[i][3]);
+        console.log('Hobi: ', arr[i][4]);
+
+        console.log('');
+    }
+}
+
+
+var input = [
+    ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+    ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+    ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+    ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+]
+
+dataHandling(input);
